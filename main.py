9d9e5888285030cbd609de9672ab05ce48069a73
ClothAtTheFair <bradleybsf12@gmail.com>
2020-03-30 21:32:01 -0400

    iteration 1 done, can only take 1 screenshot and append to files.

--- main.py
+++ main.py
@@ -6,11 +6,8 @@ import argparse
 def take_in_user_inputs():
     print('Welcome to Projects to List (name under review) by Bradley Faircloth.')
 
-    newOrOld = input("Do you need a new file? Please type yes or no ")
-    path = input("Please give the file path for either where you want the file or where it is currently located ")
     projectName = input("Please input the name of the project ")
-    screenshots = input("Please give the name of the screenshot(s) with commas to separate multiple ones. " 
-    + " The first screenshot will be considered the thumbnail ")
+    screenshots = input("Please give the name of the screenshot you wish to be the thumbnail ")
 
     # There can be more than one screenshot so the type checker should be more robust
     checked_screenshots = type_checker(screenshots, "picture")
@@ -18,18 +15,29 @@ def take_in_user_inputs():
     description = input("Please give the description for the project.")
     url = input("Please give the url for the project ")
 
-    inputs = dict(newFile = newOrOld.lower(), filePath = path, name = projectName, screenshots = checked_screenshots,
+    inputs = dict(name = projectName, screenshots = checked_screenshots,
                  description = description, url = url)
 
     return inputs
 
-def check_for_JSON(file_path):
-    #check to make sure the file is where the path mentions.
-    #if the path is there return else throw an error
-    pass
+def load_JSON(json_file):
+    with open(json_file, 'r') as openfile:
 
-def write_to_JSON(inputs):
-    pass
+        json_object = json.load(openfile)
+    print(json_object)
+    return json_object
+
+def write_to_JSON(inputs, file_name):
+    with open(file_name) as json_file:
+        data = json.load(json_file)
+
+        temp = data['projects']
+
+        temp.append(inputs)
+
+    with open(file_name, 'w') as f:
+        json.dump(data, f, indent=4)
+    
 
 def save_JSON_to_location(path):
     pass
@@ -39,13 +47,13 @@ def type_checker(input, type):
         fileExtension = input[-3::]
         # fileExtension should be png, svg, or jpg else it reject it
         if fileExtension.lower() == "png" or fileExtension == "svg" or fileExtension == "jpg":
-            return fileExtension.lower()
+            return input.lower()
         else:
             raise TypeError("screenshots are not the right file extension")
 
     elif type.lower() == "url":
         pass 
-        #Let's get to this Wednesdays
+        #Let's get to this Wednesday
 
     elif type.lower() == "path":
         pass
@@ -53,11 +61,17 @@ def type_checker(input, type):
 
 
 if __name__ == "__main__":
-    furst = take_in_user_inputs()
-    if furst["newFile"] == "no":
-        check_for_JSON(furst["filePath"])
+    user_input = input("Hello, do you wish to load a json file or add to an existing one? Please say load or add to continue ")
+    file_name = input("What is the file name? Please add .json on the end ")
+    if user_input.lower() == "load":
+        load_JSON(file_name)
+    elif user_input.lower() == "add":
+        first = take_in_user_inputs()
+        data = write_to_JSON(first, file_name)
     else:
-        write_to_JSON(furst)
+        print("I'm sorry, that isn't one of the options")
+        exit(1)
+
 
     
 
--- testMain.py
+++ testMain.py
@@ -4,21 +4,18 @@ from main import *
 class TestProjectListing:
 
     def test_file_can_be_loaded(self):
-        test_file = "./test.json"
-        good_file = check_for_JSON(test_file)
-        load_JSON(good_file)
-        # From this there should be a status code to make sure it is good
-
-    def test_file_is_found(self):
-        test_file = "./test.json"
-        actual = check_for_JSON(test_file)
-        assert actual == True
+        test_file = "test.json"
+        actual = load_JSON(test_file)
+        assert type(actual) == dict
 
     def test_user_inputs_correctly_formatted(self):
-        inputs = dict(file = "./test.json", screenshots = "test.png", name="test", description="yikes", url="bradleycodes.dev")
-        ahh = take_in_user_inputs(inputs)
+        test_file = "test.json"
+        inputs = dict(screenshots = "test.png", name="test", description="yikes", url="bradleycodes.dev")
+        write_to_JSON(inputs, test_file)
         
 
-    def test_wrong_inputs_handled(self):
-        options = dict(file="./test.csv", screenshots= "test.csv", name= "test", description="yikes forever", url ="www.yikes.org")
-        take_in_user_inputs(options)
+    def test_wrong_screenshot_type_handled(self):
+        screenshots = "test.csv"
+        with pytest.raises(TypeError):
+            type_checker(screenshots, "picture")
+
